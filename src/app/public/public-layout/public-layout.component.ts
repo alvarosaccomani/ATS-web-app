b6ce164f0ba503f4703f74e1d3bc765b54599313
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-17 12:24:47 -0300

    i add public-layout component to project

---
+++ src/app/public/public-layout/public-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [],
+  templateUrl: './public-layout.component.html',
+  styleUrl: './public-layout.component.scss',
+})
+export class PublicLayoutComponent {}
