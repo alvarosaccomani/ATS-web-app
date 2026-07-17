7f70ab40a6dc95758e60ddccd302e5f2c003afa7
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-17 16:31:21 -0300

    i add backoffice-suite route to user.routes.ts

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { UserLayoutComponent } from './user-layout/user-layout.component';
+import { BackofficeSuiteComponent } from './backoffice-suite/backoffice-suite.component';
 
 export const USER_ROUTES: Routes = [
     {
         path: '',
         component: UserLayoutComponent,
-        children: []
+        children: [
+            { path: 'backoffice-suite', component: BackofficeSuiteComponent}
+        ]
     }
 ];
