77e147001dd251b15c0cc8eff5ad53d378e6e721
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-18 09:19:53 -0300

    i add cookie-utils to auth interceptor

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,7 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { getCookie } from '../utils/cookie-utils';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('ats_token');
+  const token = getCookie('ats_token');
 
   if (token) {
     const clonedReq = req.clone({
