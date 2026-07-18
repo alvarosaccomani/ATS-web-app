019a0f77abcd6f58b503793b228730a4cd22c77b
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-18 11:41:01 -0300

    i add methods getUsers, getUserById, saveUser, updateUser and deleteUser to users service

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,4 +1,38 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-@Service()
-export class UsersService {}
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // Métodos para la gestión privada de perfiles y usuarios (CRUD)
+  public getUsers(filter?: string, page?: number, perPage?: number): Observable<any> {
+    let url = `${environment.apiUrl}users`;
+    if (filter) url += `/${filter}`;
+    if (page && perPage) url += `/${page}/${perPage}`;
+    return this.http.get<any>(url);
+  }
+
+  public getUserById(usr_uuid: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}user/${usr_uuid}`);
+  }
+
+  public saveUser(userData: any): Observable<any> {
+    return this.http.post<any>(`${environment.apiUrl}user`, userData);
+  }
+
+  public updateUser(usr_uuid: string, userData: any): Observable<any> {
+    return this.http.put<any>(`${environment.apiUrl}user/${usr_uuid}`, userData);
+  }
+
+  public deleteUser(usr_uuid: string): Observable<any> {
+    return this.http.delete<any>(`${environment.apiUrl}user/${usr_uuid}`);
+  }
+}
