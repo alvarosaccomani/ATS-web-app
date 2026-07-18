b771fcc06f8fd290f79c4d1428f62d48c4988364
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-17 23:22:02 -0300

    i add login route to auth.routes.ts

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const AUTH_ROUTES: Routes = [
     {
@@ -9,6 +10,7 @@ export const AUTH_ROUTES: Routes = [
         component: AuthLayoutComponent,
         children: [
             { path: 'register', component: RegisterComponent},
+            { path: 'login', component: LoginComponent},
         ]
     }
 ];
