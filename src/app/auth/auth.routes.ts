dc1de273bdd9ceeedaeb2bf745b060245c77d052
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-17 22:35:37 -0300

    i add register route to auth.routes.ts

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { RegisterComponent } from './register/register.component';
 
 export const AUTH_ROUTES: Routes = [
     {
         path: '',
         component: AuthLayoutComponent,
-        children: []
+        children: [
+            { path: 'register', component: RegisterComponent},
+        ]
     }
 ];
