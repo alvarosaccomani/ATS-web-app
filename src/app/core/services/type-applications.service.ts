76fa300c948da19f210407fdbc5f673dd9939eca
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-20 12:28:41 -0300

    i add methods getTypeApplications, getTypeApplicationById, saveTypeApplication, updateTypeApplication and deleteTypeApplication to type-applications service

--- src/app/core/services/type-applications.service.ts
+++ src/app/core/services/type-applications.service.ts
@@ -1,4 +1,44 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-@Service()
-export class TypeApplicationsService {}
+export interface TypeApplicationItem {
+  tapp_uuid: string;
+  tapp_cod: string;
+  tapp_name: string;
+  tapp_description: string;
+  tapp_bkcolor: string;
+  tapp_frcolor: string;
+  tapp_active: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TypeApplicationsService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getTypeApplications(): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}typeapplications`);
+  }
+
+  public getTypeApplicationById(tapp_uuid: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}typeapplication/${tapp_uuid}`);
+  }
+
+  public saveTypeApplication(typeAppData: any): Observable<any> {
+    return this.http.post<any>(`${environment.apiUrl}typeapplication`, typeAppData);
+  }
+
+  public updateTypeApplication(tapp_uuid: string, typeAppData: any): Observable<any> {
+    return this.http.put<any>(`${environment.apiUrl}typeapplication/${tapp_uuid}`, typeAppData);
+  }
+
+  public deleteTypeApplication(tapp_uuid: string): Observable<any> {
+    return this.http.delete<any>(`${environment.apiUrl}typeapplication/${tapp_uuid}`);
+  }
+}
