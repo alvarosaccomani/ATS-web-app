8f3cb6498a8c28f009f0e1248810a18b53bd8822
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-21 13:37:45 -0300

    i add methods getUserAuthLogs. getUserAuthLogsByUserId and getDetailUserAuthLog to user-auth-logs service

--- src/app/core/services/user-auth-logs.service.ts
+++ src/app/core/services/user-auth-logs.service.ts
@@ -1,4 +1,39 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-@Service()
-export class UserAuthLogsService {}
+export interface UserAuthLogItem {
+  usraulo_uuid: string;
+  usr_uuid: string;
+  app_uuid: string;
+  usraulo_action: 'LOGIN_SUCCESS' | 'LOGIN_FAILED' | 'LOGOUT' | 'TOKEN_REFRESH';
+  usraulo_ipaddress?: string;
+  usraulo_useragent?: string;
+  usraulo_failurereason?: string;
+  usraulo_createdat: string;
+  user?: any;
+  application?: any;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthLogsService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getUserAuthLogs(): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}user-auth-logs`);
+  }
+
+  public getUserAuthLogsByUserId(usr_uuid: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}user-auth-logs/user/${usr_uuid}`);
+  }
+
+  public getDetailUserAuthLog(usraulo_uuid: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}user-auth-log/${usraulo_uuid}`);
+  }
+}
