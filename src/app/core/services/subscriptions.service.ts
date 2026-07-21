65914b97729db0e6ccd1ed3967da0005fd9f3886
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-21 12:04:45 -0300

    i add methods getSubscriptions, getSubscriptionsBySubscriber, getSubscriptionById, saveSubscription, updateSubscription and deleteSubscription to subscriptions service

--- src/app/core/services/subscriptions.service.ts
+++ src/app/core/services/subscriptions.service.ts
@@ -1,4 +1,54 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-@Service()
-export class SubscriptionsService {}
+export interface SubscriptionItem {
+  sub_uuid: string;
+  pla_uuid: string;
+  app_uuid: string;
+  sub_subscribertype: 'USER' | 'COMPANY';
+  usr_uuid?: string;
+  cmp_uuid?: string;
+  sub_status: 'ACTIVE' | 'PAST_DUE' | 'CANCELLED' | 'TRIAL';
+  sub_startsat: string;
+  sub_renewsat?: string;
+  sub_endsat?: string;
+  sub_active?: boolean;
+  plan?: any;
+  application?: any;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubscriptionsService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getSubscriptions(): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}subscriptions`);
+  }
+
+  public getSubscriptionsBySubscriber(type: 'USER' | 'COMPANY', id: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}subscriptions/subscriber/${type}/${id}`);
+  }
+
+  public getSubscriptionById(sub_uuid: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}subscription/${sub_uuid}`);
+  }
+
+  public saveSubscription(subData: any): Observable<any> {
+    return this.http.post<any>(`${environment.apiUrl}subscription`, subData);
+  }
+
+  public updateSubscription(sub_uuid: string, subData: any): Observable<any> {
+    return this.http.put<any>(`${environment.apiUrl}subscription/${sub_uuid}`, subData);
+  }
+
+  public deleteSubscription(sub_uuid: string): Observable<any> {
+    return this.http.delete<any>(`${environment.apiUrl}subscription/${sub_uuid}`);
+  }
+}
