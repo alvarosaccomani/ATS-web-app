2a80044377b8245d80f16b8ee0bd23aa8d87e0cb
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-21 23:12:33 -0300

    i add methods getTransactions, getTransactionsBySubscriber, getTransactionById and saveTransaction to transactions service

--- src/app/core/services/transactions.service.ts
+++ src/app/core/services/transactions.service.ts
@@ -1,4 +1,52 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-@Service()
-export class TransactionsService {}
+export interface TransactionItem {
+  trn_uuid: string;
+  usr_uuid?: string;
+  sub_uuid?: string;
+  cmp_uuid?: string;
+  app_uuid: string;
+  trn_provider: string;
+  trn_providerid?: string;
+  trn_amount: number;
+  trn_currency: string;
+  trn_platformfee: number;
+  trn_netamount: number;
+  trn_status: 'PENDING' | 'APPROVED' | 'REJECTED' | 'REFUNDED';
+  trn_paymentmethod?: string;
+  trn_description?: string;
+  trn_metadata?: string;
+  trn_createdat: string;
+  user?: any;
+  company?: any;
+  application?: any;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionsService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getTransactions(): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}transactions`);
+  }
+
+  public getTransactionsBySubscriber(type: 'USER' | 'COMPANY', id: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}transactions/subscriber/${type}/${id}`);
+  }
+
+  public getTransactionById(trn_uuid: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}transaction/${trn_uuid}`);
+  }
+
+  public saveTransaction(trnData: any): Observable<any> {
+    return this.http.post<any>(`${environment.apiUrl}transaction`, trnData);
+  }
+}
