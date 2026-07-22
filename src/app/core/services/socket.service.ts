2181acb3eea548efbdffc3e511e5a5cca479379c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-22 09:08:43 -0300

    i add methods onEvent and emitEvent to socket service

--- src/app/core/services/socket.service.ts
+++ src/app/core/services/socket.service.ts
@@ -1,4 +1,38 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-@Service()
-export class SocketService {}
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket: Socket;
+
+  constructor() {
+    // Extrae la URL base del servidor (ej: http://localhost:3005) a partir de apiUrl
+    const serverUrl = environment.apiUrl.replace(/\/api\/$/, '') || 'http://localhost:3005';
+    this.socket = io(serverUrl, {
+      transports: ['websocket'],
+      autoConnect: true
+    });
+  }
+
+  // Escuchar cualquier evento proveniente del Socket Server
+  public onEvent<T>(eventName: string): Observable<T> {
+    return new Observable<T>((observer) => {
+      this.socket.on(eventName, (data: T) => {
+        observer.next(data);
+      });
+
+      return () => {
+        this.socket.off(eventName);
+      };
+    });
+  }
+
+  // Emitir un evento hacia el servidor
+  public emitEvent(eventName: string, data: any): void {
+    this.socket.emit(eventName, data);
+  }
+}
