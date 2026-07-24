e9f3381fa7fac9b835e183cbc2bf61698b8ffa9f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-24 08:46:13 -0300

    i add sso-callback component to project

---
+++ src/app/auth/sso-callback/sso-callback.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SsoCallbackComponent } from './sso-callback.component';
+
+describe('SsoCallbackComponent', () => {
+  let component: SsoCallbackComponent;
+  let fixture: ComponentFixture<SsoCallbackComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SsoCallbackComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SsoCallbackComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/auth/sso-callback/sso-callback.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sso-callback',
+  imports: [],
+  templateUrl: './sso-callback.component.html',
+  styleUrl: './sso-callback.component.scss',
+})
+export class SsoCallbackComponent {}
