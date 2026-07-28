20d85bdcb0150e7f42cb439a8661ca9fd8c5affe
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-28 15:45:57 -0300

    i add backoffice-suite/applications route to user.routes.ts

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -6,6 +6,7 @@ import { BackofficeSuiteComponent } from './backoffice-suite/backoffice-suite.co
 import { LauncherTabComponent } from './launcher-tab/launcher-tab.component';
 import { AuditTabComponent } from './audit-tab/audit-tab.component';
 import { SubscriptionsTabComponent } from './subscriptions-tab/subscriptions-tab.component';
+import { ApplicationsTabComponent } from './applications-tab/applications-tab.component';
 
 export const USER_ROUTES: Routes = [
     {
@@ -16,7 +17,8 @@ export const USER_ROUTES: Routes = [
             { path: 'backoffice-suite', component: BackofficeSuiteComponent},
             { path: 'backoffice-suite/launcher', component: LauncherTabComponent},
             { path: 'backoffice-suite/audit', component: AuditTabComponent},
-            { path: 'backoffice-suite/subscriptions', component: SubscriptionsTabComponent}
+            { path: 'backoffice-suite/subscriptions', component: SubscriptionsTabComponent},
+            { path: 'backoffice-suite/applications', component: ApplicationsTabComponent}
         ]
     }
 ];
