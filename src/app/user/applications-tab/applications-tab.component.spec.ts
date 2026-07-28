505250e98b20a64ac33cb586172a71bca8f6e6da
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-28 15:45:33 -0300

    i add applications-tab component to project

---
+++ src/app/user/applications-tab/applications-tab.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApplicationsTabComponent } from './applications-tab.component';
+
+describe('ApplicationsTabComponent', () => {
+  let component: ApplicationsTabComponent;
+  let fixture: ComponentFixture<ApplicationsTabComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ApplicationsTabComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ApplicationsTabComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/user/applications-tab/applications-tab.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applications-tab',
+  imports: [],
+  templateUrl: './applications-tab.component.html',
+  styleUrl: './applications-tab.component.scss',
+})
+export class ApplicationsTabComponent {}
