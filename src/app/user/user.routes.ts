d11de96ed115451bb4bb29b65017d875bc7deea0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-28 15:42:43 -0300

    i add backoffice-suite/audit route to user.routes.ts

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -4,6 +4,7 @@ import { authGuard } from '../core/guards/auth-guard';
 import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { BackofficeSuiteComponent } from './backoffice-suite/backoffice-suite.component';
 import { LauncherTabComponent } from './launcher-tab/launcher-tab.component';
+import { AuditTabComponent } from './audit-tab/audit-tab.component';
 
 export const USER_ROUTES: Routes = [
     {
@@ -12,7 +13,8 @@ export const USER_ROUTES: Routes = [
         canActivate: [authGuard],
         children: [
             { path: 'backoffice-suite', component: BackofficeSuiteComponent},
-            { path: 'backoffice-suite/launcher', component: LauncherTabComponent}
+            { path: 'backoffice-suite/launcher', component: LauncherTabComponent},
+            { path: 'backoffice-suite/audit', component: AuditTabComponent}
         ]
     }
 ];
