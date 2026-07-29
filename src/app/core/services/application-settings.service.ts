5089ee5200457e7239f38b51337091a9d63a9705
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-28 21:19:55 -0300

    i add methods getApplicationsSettings, saveApplicationSetting and updateApplicationSetting to application-settings service

--- src/app/core/services/application-settings.service.ts
+++ src/app/core/services/application-settings.service.ts
@@ -1,4 +1,37 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-@Service()
-export class ApplicationSettingsService {}
+export interface ApplicationSettingInterface {
+  app_uuid: string;
+  apps_uuid: string;
+  apps_key: string;
+  apps_parameter: string;
+  apps_description: string;
+  apps_value: string;
+  apps_datatype: string;
+  apps_options: string;
+  apps_group: string;
+  apps_createdat: Date;
+  apps_updatedat: Date;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicationSettingsService {
+  constructor(private http: HttpClient) {}
+
+  public getApplicationsSettings(app_uuid: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}application/${app_uuid}/settings`);
+  }
+
+  public saveApplicationSetting(payload: any): Observable<any> {
+    return this.http.post<any>(`${environment.apiUrl}application/${payload.app_uuid}/settings`, payload);
+  }
+
+  public updateApplicationSetting(payload: any): Observable<any> {
+    return this.http.put<any>(`${environment.apiUrl}application/${payload.app_uuid}/settings/${payload.apps_uuid}`, payload);
+  }
+}
