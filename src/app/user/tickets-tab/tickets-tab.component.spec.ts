94cece206f86fd56fcd4858184f801fbffe226e5
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-08-05 12:30:24 -0300

    i add tickets-tab component to project

---
+++ src/app/user/tickets-tab/tickets-tab.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TicketsTabComponent } from './tickets-tab.component';
+
+describe('TicketsTabComponent', () => {
+  let component: TicketsTabComponent;
+  let fixture: ComponentFixture<TicketsTabComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TicketsTabComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TicketsTabComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/user/tickets-tab/tickets-tab.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets-tab',
+  imports: [],
+  templateUrl: './tickets-tab.component.html',
+  styleUrl: './tickets-tab.component.scss',
+})
+export class TicketsTabComponent {}
