4d1185c7dd9d7dc49baaef54c5d587061f28f3da
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-08-05 19:40:18 -0300

    i add methods getResponsibles, assignResponsible and removeResponsible to app-responsibles service

--- src/app/core/services/app-responsibles.service.ts
+++ src/app/core/services/app-responsibles.service.ts
@@ -1,4 +1,26 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-@Service()
-export class AppResponsiblesService {}
+@Injectable({
+  providedIn: 'root'
+})
+export class AppResponsiblesService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+  
+  public getResponsibles(app_uuid: string): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}app-responsibles/${app_uuid}`);
+  }
+
+  public assignResponsible(app_uuid: string, usr_uuid: string, rol_uuid: string): Observable<any> {
+    return this.http.post<any>(`${environment.apiUrl}app-responsible/${app_uuid}`, { usr_uuid, rol_uuid });
+  }
+
+  public removeResponsible(appres_uuid: string): Observable<any> {
+    return this.http.delete<any>(`${environment.apiUrl}app-responsible/${appres_uuid}`);
+  }
+}
